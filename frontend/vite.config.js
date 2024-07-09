71316f8efc1a0c0980e3d03c4049806dabd1fda7
Mythili <mythiliselvaraju2004@gmail.com>
2024-07-09 16:49:06 +0530

    msg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
-// vite.config.js
-export default {
-  build: {
-    outDir: 'dist'
-  },
-  base: '/frontend/',
-};
+export default defineConfig({
+  plugins: [react()]
+})
